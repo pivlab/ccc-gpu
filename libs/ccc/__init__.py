29c396e3fc14d961947d9442cba48ece65f3a5bf
Haoyu Zhang <haoyu_z@outlook.com>
2024-10-31 15:46:49 -0600

    [build]: Compile cudaAri with pybind11 successfully

--- libs/ccc/__init__.py
+++ libs/ccc/__init__.py
@@ -1,2 +1,8 @@
 # Remember to change also setup.py with the version here
 __version__ = "0.1.7"
+
+from __future__ import annotations
+
+from ._core import add, subtract
+
+__all__ = ["add", "subtract"]
