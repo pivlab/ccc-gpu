f835b23a750fa55e75f940edd003f0abb4562eb8
Haoyu Zhang <haoyu_z@outlook.com>
2024-11-02 18:12:39 -0600

    [test/kernel]: Test ari with latest C++ API

--- libs/ccc/__init__.py
+++ libs/ccc/__init__.py
@@ -1,8 +1,5 @@
+from __future__ import annotations
+
 # Remember to change also setup.py with the version here
 __version__ = "0.1.7"
 
-from __future__ import annotations
-
-from ._core import add, subtract
-
-__all__ = ["add", "subtract"]
