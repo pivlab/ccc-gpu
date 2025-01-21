5967bcf709458370379d9314f75650b65a95c7b5
Haoyu Zhang <haoyu_z@outlook.com>
2025-01-20 21:25:53 -0700

    [build]: Add camke as a conda dependency

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -13,6 +13,12 @@ conda activate ccc-gpu
 # Set the PYTHONPATH
 export PYTHONPATH=`readlink -f ./libs/`:$PYTHONPATH
 
+# Set the pybind11 path for cmake to find it
+# (Locally installed pybind11 by conda can not be found by cmake)
+PYBIND11_DIR=$(find ~/ -type d -path "*/anaconda3/pkgs/*/pybind11/share/cmake/pybind11" 2>/dev/null)
+export CMAKE_PREFIX_PATH=$PYBIND11_DIR:$CMAKE_PREFIX_PATH
+
+# Set the CUDA_HOME and LD_LIBRARY_PATH
 # Set the CUDA_HOME and LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="~/anaconda3/envs/ccc-cuda/lib/:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="~/anaconda3/envs/ccc-cuda/lib/:$LD_LIBRARY_PATH"
