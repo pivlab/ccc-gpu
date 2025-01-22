6ba6798df89314a1f324269e6023c8e0244e8d72
Haoyu Zhang <haoyu_z@outlook.com>
2025-01-21 22:03:43 -0700

    [build]: Fail to include thrust in .cuh files

--- libs/ccc_cuda_ext/coef.cuh
+++ libs/ccc_cuda_ext/coef.cuh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 namespace py = pybind11;
 
@@ -9,3 +10,12 @@ auto example_return_optional_vectors(
     bool include_second = true,
     bool include_third = true
 ) -> py::object;
+
+
+template <typename T>
+auto compute_coef(const py::array_t<T, py::array::c_style> &parts,
+                 const size_t n_features,
+                 const size_t n_parts,
+                 const size_t n_objs,
+                 const bool return_parts,
+                 std::optional<unsigned int> pvalue_n_perms = std::nullopt) -> py::object;
--- libs/ccc_cuda_ext/metrics.cuh
+++ libs/ccc_cuda_ext/metrics.cuh
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <vector>
+#include <memory>
 #include <pybind11/numpy.h>
-
+#include <thrust/device_vector.h>
 
 namespace py = pybind11;
 
 template <typename T>
-auto ari(const py::array_t<T, py::array::c_style>& parts, 
+auto ari(const py::array_t<T, py::array::c_style>& parts,
          const size_t n_features,
          const size_t n_parts,
          const size_t n_objs) -> std::vector<float>;
@@ -18,3 +19,10 @@ auto ari_core(const T* parts,
          const size_t n_features,
          const size_t n_parts,
          const size_t n_objs) -> std::vector<float>;
+
+// Declaration of the device function
+template <typename T, typename R>
+auto ari_core_device(const T *parts,
+                    const size_t n_features,
+                    const size_t n_parts,
+                    const size_t n_objs) -> std::unique_ptr<thrust::device_vector<R>>;
