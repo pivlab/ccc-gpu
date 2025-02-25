0c77c0321c221ceb48b6d46be2a5716d34933c91
Haoyu Zhang <haoyu_z@outlook.com>
2025-02-25 11:23:52 -0700

    [mics]: Test library cuml

--- tests/gpu/scratch.py
+++ tests/gpu/scratch.py
@@ -1,5 +1,15 @@
-from sklearn.metrics.cluster import adjusted_rand_score
+import numpy as np
 
-print(adjusted_rand_score([0, 0, 1, 1], [2, 1, 2, 0]))
-print(adjusted_rand_score([1, 1, 0, 0], [0, 0, 1, 2]))
-print(adjusted_rand_score([0, 1, 0, 1], [0, 0, 1, 2]))
+from cuml.metrics.cluster.adjusted_rand_index import adjusted_rand_score
+from ccc.sklearn.metrics import adjusted_rand_index
+
+n_samples = 5000
+
+in1 = np.random.randint(0, 100, size=(n_samples,), dtype=np.int32)
+in2 = np.random.randint(0, 100, size=(n_samples,), dtype=np.int32)
+
+output_gpu = adjusted_rand_score(in1, in2)
+output_cpu = adjusted_rand_index(in1, in2)
+
+print(output_gpu)
+print(output_cpu)
