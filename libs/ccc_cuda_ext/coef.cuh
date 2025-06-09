bd3cd26f44667cfead4e1f3d549a7a6fd8380c3e
Haoyu Zhang <haoyu_z@outlook.com>
2025-06-09 16:05:58 -0600

    [coef]: Organize source code

--- libs/ccc_cuda_ext/coef.cuh
+++ libs/ccc_cuda_ext/coef.cuh
@@ -18,4 +18,4 @@ auto compute_coef(const py::array_t<T, py::array::c_style> &parts,
                  const size_t n_parts,
                  const size_t n_objs,
                  const bool return_parts,
-                 std::optional<unsigned int> pvalue_n_perms = std::nullopt) -> py::object;
+                 std::optional<uint32_t> pvalue_n_perms = std::nullopt) -> py::object;
