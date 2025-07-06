0e3fc86ed5ffe54d4590c9478cf88c15aa3c7101
Haoyu Zhang <haoyu_z@outlook.com>
2025-07-06 16:16:09 -0600

    [analysis]: Generate complete upset plots for wholeblood successfully

--- nbs/03-manuscript/15_intersections/run_slurm_job.sh
+++ nbs/03-manuscript/15_intersections/run_slurm_job.sh
@@ -1,12 +1,12 @@
-# #!/bin/bash
+#!/bin/bash
 #SBATCH --job-name=CCC_GPU_Intersections
 #SBATCH --output=_tmp/CCC_GPU_Intersections.%j.out
 #SBATCH --error=_tmp/CCC_GPU_Intersections.%j.err
 #SBATCH --time=03:00:00
 #SBATCH --qos=normal
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
-#SBATCH --mem=512B
+#SBATCH --cores=8
+#SBATCH --mem=512GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=haoyu.zhang@cuanschutz.edu
 
